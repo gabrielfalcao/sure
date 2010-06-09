c4fec2fbc15e02b6daedda9e89dfc16f26e509f8
Gabriel Falcão <gabriel@nacaolivre.org>
2010-06-09 01:00:27 -0300

    fixing unicode

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ def get_packages():
 setup(name='sure',
     version=version,
     description='python-pure implementation of CSS Selectors',
-    author='Gabriel Falcão',
+    author=u'Gabriel Falcão',
     author_email='gabriel@nacaolivre.org',
     url='http://github.com/gabrielfalcao/sure',
     packages=get_packages(),

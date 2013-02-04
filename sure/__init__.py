0be8253a09d533c9203bd91ecaa6545fe28307e5
Gabriel Falcao <gabriel@nacaolivre.org>
2013-02-04 15:21:11 -0500

    release 1.1.7

--- sure/__init__.py
+++ sure/__init__.py
@@ -41,7 +41,7 @@ from sure.registry import context as _registry
 from sure.six import string_types, text_type, PY3, get_function_code
 from sure.six.moves import reduce
 
-version = '1.1.6'
+version = '1.1.7'
 
 
 not_here_error = \

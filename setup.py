1b4a98e01085e13f6d2f80d07ae0ff5f7a84b032
Timo Furrer <timo.furrer@roche.com>
2015-11-12 14:48:16 -0800

    Revert "always loading files as utf-8 in setup.py"
    This reverts commit cd511b5bd0114f26be9f03b8123ca127b4e4d080.

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@
 import ast
 import os
 import sys
-import codecs
 from setuptools import setup, find_packages
 
 
@@ -43,9 +42,10 @@ def read_version():
     return finder.version
 
 
-def local_file(*f):
-    path = os.path.join(os.path.dirname(__file__), *f)
-    return codecs.open(path, 'r', encoding='utf-8').read()
+def local_file( *f):
+    if sys.version_info < (3, 0, 0):
+        return open(os.path.join(os.path.dirname(__file__), *f)).read()
+    return open(os.path.join(os.path.dirname(__file__), *f), encoding="utf-8").read()
 
 
 install_requires = ['mock', 'six']
